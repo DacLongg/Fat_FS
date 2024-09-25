7afdb5d8c2d8358b09cdc7382b4d59c5631442ef
DacLongg <ddragon2112001@gmail.com>
2024-09-25 22:55:41 +0700

    build type

--- FatFS/FatFS/FatFs_type.h
+++ FatFS/FatFS/FatFs_type.h
@@ -1,7 +1,18 @@
 #pragma once
-
+#include "stdint.h"
 
 typedef struct
 {
 
 }FatFs_t;
+
+
+
+typedef struct
+{
+	uint16_t sector_size;
+	uint16_t total_sector;
+	uint16_t FAT_size;
+	uint8_t Clustor_size;
+
+}Fatfs_BootInfo;
